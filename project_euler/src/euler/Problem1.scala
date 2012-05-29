77f000fa975160a9c4daeee8e3be832c00833426
Antoine Ansel <antoine.ansel@gmail.com>
2012-05-29 23:49:00 +0200

    pb1

--- project_euler/src/euler/Problem1.scala
+++ project_euler/src/euler/Problem1.scala
@@ -2,7 +2,19 @@ package euler
 
 object Problem1 {
   
-  def main(args: Array[String]) = println("Hello world")
+  /**
+   * 3 + 5 + 6 + 9 + 10 + 12
+   * = 3 + 6 + 9 + 12 + 15 + 18
+   * + 5 + 10 + 15 + 20
+   * - 15 - 30 - 45... 
+   */
+  def main(args: Array[String]) =  {
+    println(sum(3, 999) + sum(5, 999) - sum(15, 999))
+  }
   
+  def sum(raison: Int, limit: Int) = {
+    val lastValue = limit - limit % raison
+    (lastValue + raison) * lastValue / (2 * raison) 
+  }
 
 }
