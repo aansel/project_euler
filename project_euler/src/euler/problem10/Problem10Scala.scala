28508ba3e755dc93268395443724cca17fec80a9
Antoine Ansel <antoine.ansel@gmail.com>
2012-06-07 23:07:00 +0200

    Problem 10

--- project_euler/src/euler/problem10/Problem10Scala.scala
+++ project_euler/src/euler/problem10/Problem10Scala.scala
@@ -13,18 +13,18 @@ object Problem10Scala {
   // Somme des nombres premiers de 1 à limit
   def sumPrimes(limit: Int): Long = {
     possiblePrimeStream()
-    	.takeWhile(_ <= limit)
-    	.filter(isPrime(_))
-    	.foldLeft(0L)(_ + _)
+      .takeWhile(_ <= limit)
+      .filter(isPrime(_))
+      .foldLeft(0L)(_ + _)
   }
   
   // Au delà de 3, tous les nombres premiers peuvent d'écrire sous la form 6k +/- 1
   // possiblePrimeStream renvoie un stream infini de nombre potentiellement premiers d'après cette règle
   def possiblePrimeStream(): Stream[Int] = {
-	  def possiblePrimeStream(k: Int): Stream[Int] = {
-	      (6 * k - 1) #:: (6 * k + 1) #:: possiblePrimeStream(k + 1)
-	  }
-	  2 #:: 3 #:: possiblePrimeStream(1)
+    def possiblePrimeStream(k: Int): Stream[Int] = {
+      (6 * k - 1) #:: (6 * k + 1) #:: possiblePrimeStream(k + 1)
+    }
+	2 #:: 3 #:: possiblePrimeStream(1)
   }
 
   // n est premier s'il n'est divisible par aucun premier de 1 à racine(n)
