00e76d08c0dfbdf6459af1462e4c66c752b11a9e
Antoine Ansel <antoine.ansel@gmail.com>
2012-06-07 23:03:02 +0200

    Problem 10

---
+++ project_euler/src/euler/problem10/Problem10Java.java
@@ -0,0 +1,52 @@
+package euler.problem10;
+
+public class Problem10Java {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Java version");
+		long t1 = System.currentTimeMillis();
+		System.out.println(getSumPrimes(2000000));
+		long t2 = System.currentTimeMillis();
+		System.out.println((t2 - t1) + " ms");
+	}
+	
+	// Somme des nombres premiers de 1 à limit
+	private static long getSumPrimes(int limit) {
+		long sum = 5;
+		int nb = 5;
+		while(nb <= limit) {
+			if (isPrime(nb)) {
+				sum += nb;
+			}
+			nb = nb + 2;
+			if (nb <= limit && isPrime(nb)) {
+				sum += nb;
+			}
+			nb = nb + 4;
+		}
+		return sum;
+	}
+	
+	
+	private static boolean isPrime(int nb) {
+		if (nb % 2 == 0 || nb % 3 == 0) {
+			return false;
+		} else {
+			int i = 5;
+			double limit = Math.sqrt(nb);
+			while(i <= limit) {
+				if (nb % i == 0) {
+					return false;
+				}
+				i = i + 2;
+				if (i <= limit && nb % i == 0) {
+					return false;
+				}
+				i = i + 4;
+			}
+			return true;
+		}
+	}
+
+}
---
+++ project_euler/src/euler/problem10/Problem10Scala.scala
@@ -0,0 +1,37 @@
+package euler.problem10
+
+object Problem10Scala {
+
+  def main(args: Array[String]): Unit = {
+    println("Scala version")
+    val t1: Long = System.currentTimeMillis() 
+    println(sumPrimes(2000000))
+    val t2: Long = System.currentTimeMillis()
+    println((t2 - t1) + " ms")
+  }
+  
+  // Somme des nombres premiers de 1 à limit
+  def sumPrimes(limit: Int): Long = {
+    possiblePrimeStream()
+    	.takeWhile(_ <= limit)
+    	.filter(isPrime(_))
+    	.foldLeft(0L)(_ + _)
+  }
+  
+  // Au delà de 3, tous les nombres premiers peuvent d'écrire sous la form 6k +/- 1
+  // possiblePrimeStream renvoie un stream infini de nombre potentiellement premiers d'après cette règle
+  def possiblePrimeStream(): Stream[Int] = {
+	  def possiblePrimeStream(k: Int): Stream[Int] = {
+	      (6 * k - 1) #:: (6 * k + 1) #:: possiblePrimeStream(k + 1)
+	  }
+	  2 #:: 3 #:: possiblePrimeStream(1)
+  }
+
+  // n est premier s'il n'est divisible par aucun premier de 1 à racine(n)
+  def isPrime(n: Int): Boolean = {
+   	val limit = Math.sqrt(n)
+    val primeSream = possiblePrimeStream().dropWhile(divisor => divisor <= limit && n % divisor != 0)
+    primeSream.first > limit
+  }
+
+}
